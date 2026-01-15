1da89789407d3dac4f41f9224e9b03f3cb0a0c3f
poserbb <admiralv09@gmail.com>
2026-01-15 23:09:11 +0300

    Лабораторная 1

---
+++ src/lab1/Calc1.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Calc1 {
+    double result1 = (46+10) * (10/3);
+    int result2 = 29 * 4 * (-15);
+    public void print(){
+        System.out.println("Значение выражения 1 = " + result1 + "\n" + "Значение выражения 2 = " + result2 + "\n");
+    }
+}
--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,17 +1,47 @@
 package lab1;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Лабораторная 1, задание 1.\n");
+        isKnowJava isKnow = new isKnowJava();
+        isKnow.print();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Лабораторная 1, задание 2.\n");
+        Calc1 calc1 = new Calc1();
+        calc1.print();
+
+        System.out.println("Лабораторная 1, задание 3.\n");
+        Calc10500 calc10500 = new Calc10500();
+        calc10500.print();
+
+        System.out.println("Лабораторная 1, задание 4.\n");
+        ThreeNumbrs tnum = new ThreeNumbrs();
+        tnum.print();
+
+        System.out.println("Лабораторная 1, задание 5.\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое число");
+        int first = scanner.nextInt();
+        System.out.println("Введите второе число");
+        int second = scanner.nextInt();
+        System.out.println("Введите третье число");
+        int third = scanner.nextInt();
+        System.out.println("\n" + "Введены числа: \n" + first + "\n" + second + "\n" + third);
+
+        System.out.println("Лабораторная 1, задание 6.\n");
+        System.out.println("Введите число");
+        int b = scanner.nextInt();
+        if (b % 2 != 0) {
+            System.out.println("\nНечётное");
+        } else if (b % 2 == 0 && b <= 100) {
+            System.out.println("\nЧётное");
+        } else if (b % 2 == 0 && b > 100) {
+            System.out.println("\nВыход за пределы диапазона");
         }
     }
 }
